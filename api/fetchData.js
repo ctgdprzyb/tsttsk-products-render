8133c92bb5adf233b6b4a587467beb81962b976c
ctgdprzyb <126746748+ctgdprzyb@users.noreply.github.com>
2023-09-06 15:52:29 +0200

    create a new function for filtering products

--- api/fetchData.js
+++ api/fetchData.js
@@ -2,7 +2,7 @@ const URL = 'https://dummyjson.com/products';
 
 // 'https://dummyjson.com/products?limit=10&skip=10&select=title,price'
 
-export async function fetchWorldState(skippedProducts = 0) {
+export async function fetchProductList(skippedProducts = 0) {
   return fetch(URL + '?limit=10&skip=' + skippedProducts)
     .then(response => {
       if (!response.ok) {
@@ -10,6 +10,18 @@ export async function fetchWorldState(skippedProducts = 0) {
       }
 
       console.log('returning a response')
+      return response.json();
+    })
+}
+
+export async function fetchProductsByName(query, skippedProducts = 0) {
+  return fetch(URL + '/search?q=' + query + '?limit=10&skip=' + skippedProducts)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('cant fetch');
+      }
+
+      console.log('returning search result')
       return response.json();
     })
 }
