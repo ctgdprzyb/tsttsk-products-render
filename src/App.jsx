a55f5a1a47ab39f88105a4a1944759d92f75ccd3
ctgdprzyb <126746748+ctgdprzyb@users.noreply.github.com>
2023-09-06 13:58:32 +0200

    removed template html

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import './App.css'
 import { fetchWorldState } from '../api/fetchData'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   useEffect(() => {
     fetchWorldState()
@@ -18,26 +18,7 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more baby
-      </p>
+      <h1 className='text-3xl font-bold'>Products app</h1>
     </>
   )
 }
