1e947acc6b965285b415ef318091f958d4f3e1dc
ctgdprzyb <126746748+ctgdprzyb@users.noreply.github.com>
2023-09-06 13:48:45 +0200

    add code to fetch products

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { fetchWorldState } from '../api/fetchData'
 
 function App() {
   const [count, setCount] = useState(0)
+
+  useEffect(() => {
+    fetchWorldState()
+          .then(response => {
+            console.log('setting worldstate');
+            setProducts(response);
+          })
+          .catch(() => console.log('fetch fail'));
+  }, []);
 
   return (
     <>
