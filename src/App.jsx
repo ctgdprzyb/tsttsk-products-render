b43c7b6e7ec012ab1ab540800a99102054b7c28b
ctgdprzyb <126746748+ctgdprzyb@users.noreply.github.com>
2023-09-06 14:05:44 +0200

    render first 10 elements in a table

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,37 @@ function App() {
 
   return (
     <>
-      <h1 className='text-3xl font-bold'>Products app</h1>
+      <h1 className='text-3xl font-bold mb-8'>Products app</h1>
+      <div className='border-double border-rose-700 border-8 rounded-md'>
+        {!products ? <p>Loading...</p> :
+        <table className='table-auto w-full'>
+          <thead className='bg-slate-400'>
+            <tr>
+              <th>ID</th>
+              <th>Title</th>
+              <th>Price</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {products.products.map((product) => {
+              return (<tr key={product.id}>
+                <td>{product.id}</td>
+                <td>{product.title}</td>
+                <td>{product.price}</td>
+                <td className='text-center'>
+                  <select name="Actions" id="actions">
+                    <option value="">--</option>
+                    <option value="info">Info</option>
+                    <option value="delete">Delete</option>
+                  </select>
+                </td>
+              </tr>)
+            })}
+          </tbody>
+        </table>
+        }
+      </div>
     </>
   )
 }
