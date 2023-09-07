3203c3dde12c834e849af4ae0d8552fb3c280f3c
ctgdprzyb <126746748+ctgdprzyb@users.noreply.github.com>
2023-09-07 06:10:29 +0200

    prettied up the info modal slightly

--- src/components/productDetails.jsx
+++ src/components/productDetails.jsx
@@ -16,19 +16,25 @@ export function ProductDetails({ product, setInspectedProduct }) {
   className="h-full w-full bg-opacity-80 absolute inset-0 bg-black flex"
   >
     <div className="
-    align-middle p-3 m-auto opacity rounded-md bg-white max-h-96"
+    max-w-lg align-middle p-5 m-auto opacity rounded-md bg-white max-h-96"
     >
       {prodDetails
       ? <>
       <div className="flex justify-between">
-        <h2 className="text-2xl">{prodDetails.title}</h2>
+        <h2 className="text-2xl text-left">{prodDetails.title}</h2>
         <button
-         onClick={() => setInspectedProduct(0)}
+        className="relative text-right -top-4 -right-2"
+          onClick={() => setInspectedProduct(0)}
         >
           x
         </button>
       </div>
       <p>{prodDetails.description}</p>
+      <div className="flex justify-center">
+        {prodDetails.images.map(image => (
+        <img key={image} src={image} className="h-12"></img>
+        ))}
+      </div>
       <p>Price: {prodDetails.price} PLN</p>
       </>
       : <p>Loading...</p>}
