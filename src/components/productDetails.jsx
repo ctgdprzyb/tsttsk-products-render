5cdac5f140bef50d29a7b36341b13cf965228aff
ctgdprzyb <126746748+ctgdprzyb@users.noreply.github.com>
2023-09-07 05:49:20 +0200

    add product info to the modal

--- src/components/productDetails.jsx
+++ src/components/productDetails.jsx
@@ -1,16 +1,38 @@
 /* eslint-disable react/prop-types */
 
+import { useEffect, useState } from "react"
+import { fetchProductDetails } from "../../utils/fetchData"
+
 export function ProductDetails({ product, setInspectedProduct }) {
+  const [prodDetails, setProdDetails] = useState();
+
+  useEffect(() => {
+    fetchProductDetails(product)
+      .then(response => setProdDetails(response))
+  }, [product])
 
   return (
   <div
-  className="h-full w-full opacity-50 absolute inset-0 bg-black"
+  className="h-full w-full bg-opacity-80 absolute inset-0 bg-black flex"
   >
-    <div className="container mx-auto rounded-md bg-slate-50">
-    <p>
-    Hello! This is {product}
-    </p>
-      <button onClick={() => setInspectedProduct(0)}>CLOSE</button>
+    <div className="
+    align-middle p-3 m-auto opacity rounded-md bg-white max-h-96"
+    >
+      {prodDetails
+      ? <>
+      <div className="flex justify-between">
+        <h2 className="text-2xl">{prodDetails.title}</h2>
+        <button
+         onClick={() => setInspectedProduct(0)}
+        >
+          x
+        </button>
+      </div>
+      <p>{prodDetails.description}</p>
+      <p>Price: {prodDetails.price} PLN</p>
+      </>
+      : <p>Loading...</p>}
+    
     </div>
   </div>
   )
